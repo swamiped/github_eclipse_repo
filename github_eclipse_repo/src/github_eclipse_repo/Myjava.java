9ede7d8156eefc8e1dd290eb52dbb6b66d601b58
swamiped <swamipedireddi99@gmail.com>
2021-11-11 17:19:53 +0530

    My changes

---
+++ github_eclipse_repo/src/github_eclipse_repo/Myjava.java
@@ -0,0 +1,10 @@
+package github_eclipse_repo;
+
+public class Myjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java users");
+	}
+
+}
